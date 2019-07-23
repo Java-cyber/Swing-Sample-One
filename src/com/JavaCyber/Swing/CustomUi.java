a26f5dd14265ad884cc8e556721ec2f63655f7a0
Java-cyber <backiran52@gmail.com>
2019-07-23 05:34:31 -0700

    Updated

---
+++ src/com/JavaCyber/Swing/CustomUi.java
@@ -0,0 +1,84 @@
+package com.JavaCyber.Swing;
+
+//Optimize Import
+import javax.swing.*;
+
+
+
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class CustomUi extends JFrame {
+
+    JButton btn;
+    JTextField numOne;
+    JTextField numTwo;
+    JLabel showlb;
+
+
+    public CustomUi(){
+
+        btn = new JButton("Sum!");
+        numOne = new JTextField(10);
+        numTwo = new JTextField(10);
+        showlb = new JLabel();
+
+        add(btn);
+        add(numOne);
+        add(numTwo);
+        add(showlb);
+
+
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+
+                try {
+
+
+                    int nuOne = Integer.parseInt(numOne.getText());
+                    int nuTwo = Integer.parseInt(numTwo.getText());
+                    int sum = nuOne + nuTwo;
+                    showlb.setText(String.valueOf(sum));
+
+
+                }catch (Exception e)
+                {
+                    JOptionPane.showMessageDialog(
+                            CustomUi.this
+                            ,
+                            "فیلد نباید خالی باید"
+                            ,
+                            "خطا"
+                            ,
+                            JOptionPane.ERROR_MESSAGE
+                    );
+
+                    showlb.setText(" ");
+
+                }
+
+
+
+
+            }
+        });
+
+        Dimension d = new Dimension();
+        d.width = 500;
+        d.height = 500;
+
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setSize(d);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+    }
+}
