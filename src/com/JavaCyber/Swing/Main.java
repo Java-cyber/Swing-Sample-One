5aca4e3d7b491e64121fe5395eb3eae3a1e71db5
Java-cyber <53215522+Java-cyber@users.noreply.github.com>
2019-07-23 05:51:14 -0700

    updated

--- src/com/JavaCyber/Swing/Main.java
+++ src/com/JavaCyber/Swing/Main.java
@@ -3,6 +3,11 @@ package com.JavaCyber.Swing;
 public class Main {
 
     public static void main(String[] args) {
-
+          SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CustomUi customUi = new CustomUi();
+            }
+        });
     }
 }
